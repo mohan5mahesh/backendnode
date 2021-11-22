cc53af5261d19d0b2ff750a354ea5f7c092e49c5
Mohan <mohan5mahesh@gmail.com>
2021-11-22 13:54:43 +0530

    add push and get method

--- index.js
+++ index.js
@@ -1,62 +1,92 @@
-import express from "express";
+import express from "express"; //default export
 // const express = require("express");
+import { MongoClient } from "mongodb"; //named export
 const app = express();
 const PORT = 5000;
 app.use(express.json()); //middleware all the body parsed has json
 
-const users = [
-  {
-    createdAt: "2021-08-23T05:57:51.816Z",
-    name: "Shaun Breitenbergs",
-    avatar: "https://cdn.fakercloud.com/avatars/fjaguero_128.jpg",
-    id: "1",
-  },
-  {
-    createdAt: "2021-08-22T17:05:46.069Z",
-    name: "Elijah D'Amore III",
-    avatar: "https://cdn.fakercloud.com/avatars/kaysix_dizzy_128.jpg",
-    id: "2",
-  },
-  {
-    createdAt: "2021-08-22T21:26:00.130Z",
-    name: "Donna Hansen",
-    avatar: "https://cdn.fakercloud.com/avatars/bighanddesign_128.jpg",
-    id: "7",
-  },
-  {
-    createdAt: "2021-08-22T12:37:31.846Z",
-    name: "Jared Sanford Sr.",
-    avatar: "https://cdn.fakercloud.com/avatars/jacksonlatka_128.jpg",
-    id: "8",
-  },
-  {
-    createdAt: "2021-08-23T01:02:10.225Z",
-    name: "Angelo Keeling",
-    avatar: "https://cdn.fakercloud.com/avatars/superoutman_128.jpg",
-    id: "9",
-  },
-  {
-    createdAt: "2021-08-22T09:42:16.459Z",
-    name: "Marcella Abbott",
-    avatar: "https://cdn.fakercloud.com/avatars/gcmorley_128.jpg",
-    id: "10",
-  },
-];
 //connection to DB in local
-const url = "mongodb://localhost";
+const MONGO_URL = "mongodb://localhost";
+// const MONGO_URL = "mongodb+srv://mohan:mohan123@cluster0.c2ecx.mongodb.net";
+async function createConnection() {
+  const client = new MongoClient(MONGO_URL);
+  await client.connect();
+  return client;
+}
 
 app.get("/", (request, response) => {
   response.send("Hello World!");
 });
 
-app.get("/users", (request, response) => {
-  response.send(users);
+app.get("/users", async (request, response) => {
+  const client = await createConnection();
+  const result = await client
+    .db("flipkart")
+    .collection("users")
+    .find({})
+    .toArray();
+  response.send(result);
 });
 
-app.post("/user", (request, response) => {
-  const data = request.body;
-  console.log(data);
-  response.send({ msg: "Created" });
+app.get("/users/:id", async (request, response) => {
+  const { id } = request.params;
+  console.log({ id });
+  const client = await createConnection();
+  const result = await client
+    .db("flipkart")
+    .collection("users")
+    .find({ id: id })
+    .toArray();
+  response.send(result);
 });
+
+app.post("/users", async (request, response) => {
+  const usersdata = request.body;
+  console.log(usersdata);
+  const client = await createConnection();
+  const result = await client
+    .db("flipkart")
+    .collection("users")
+    .insertMany(request.body);
+  response.send(result);
+});
+
+app.post("/user", async (request, response) => {
+  const userdata = request.body;
+  console.log(userdata);
+  const client = await createConnection();
+  const result = await client
+    .db("flipkart")
+    .collection("users")
+    .insertOne(request.body);
+  response.send(result);
+});
+
+app.delete("/users/:id", async (request, response) => {
+  const { id } = request.body;
+  console.log({ id });
+  const client = await createConnection();
+  const result = await client
+    .db("flipkart")
+    .collection("users")
+    .deleteOne({ id: id });
+  response.send(result);
+});
+
+app.get("/brands", async (request, response) => {
+  const client = await createConnection();
+  const result = await client
+    .db("flipkart")
+    .collection("brands")
+    .find({})
+    .toArray();
+  response.send(result);
+});
+
+// app.post("/user", (request, response) => {
+//   const data = request.body;
+//   console.log(data);
+//   response.send({ msg: "Created" });
+// });
 
 app.listen(PORT, () => console.log("This is port is start", PORT));
