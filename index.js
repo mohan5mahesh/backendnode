056f58e7dabc57f502df58ad98ae68edc7a6b12c
Mohan <mohan5mahesh@gmail.com>
2021-11-25 20:17:56 +0530

    add password

--- index.js
+++ index.js
@@ -3,16 +3,29 @@ import express from "express"; //default export
 import { MongoClient } from "mongodb"; //named export
 import dotenv from "dotenv";
 import cors from "cors";
+import { ObjectId } from "mongodb";
+import bcrypt from "bcrypt";
 const app = express();
 
 app.use(cors()); //allow the cross origin request
 app.use(express.json()); //middleware all the body parsed has json
 dotenv.config();
 
-//connection to DB in local
 // MONGO_URL = "mongodb://localhost"
 const PORT = process.env.PORT;
 const MONGO_URL = process.env.MONGO_URL;
+
+//Generation the hashed password
+async function genPassword(password) {
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+  console.log(hashedPassword);
+  return hashedPassword;
+}
+// genPassword("password@123");
+// genPassword("password@123");
+
+//connection to DB in local
 async function createConnection() {
   const client = new MongoClient(MONGO_URL);
   await client.connect();
@@ -40,7 +53,7 @@ app.get("/users/:id", async (request, response) => {
   const result = await client
     .db("flipkart")
     .collection("users")
-    .find({ id: id })
+    .find({ _id: ObjectId(id) })
     .toArray();
   response.send(result);
 });
@@ -66,17 +79,31 @@ app.post("/user", async (request, response) => {
     .insertOne(request.body);
   response.send(result);
 });
+//signup the new user
+app.post("/user/signup", async (request, response) => {
+  const { name, password, avatar } = request.body;
+  console.log(name, password, avatar);
+  const hashedPassword = await genPassword(password);
+  const client = await createConnection();
+  const result = await client.db("flipkart").collection("users").insertOne({
+    name: name,
+    avatar: avatar,
+    password: hashedPassword,
+    createdAt: new Date().toISOString(),
+  });
+  response.send(result);
+});
 
-// app.delete("/users/:id", async (request, response) => {
-//   const { id } = request.body;
-//   console.log({ id });
-//   const client = await createConnection();
-//   const result = await client
-//     .db("flipkart")
-//     .collection("users")
-//     .deleteOne({ id: id });
-//   response.send(result);
-// });
+app.delete("/users/:id", async (request, response) => {
+  const { id } = request.params;
+  console.log({ id });
+  const client = await createConnection();
+  const result = await client
+    .db("flipkart")
+    .collection("users")
+    .deleteOne({ _id: ObjectId(id) });
+  response.send(result);
+});
 
 app.get("/brands", async (request, response) => {
   const client = await createConnection();
